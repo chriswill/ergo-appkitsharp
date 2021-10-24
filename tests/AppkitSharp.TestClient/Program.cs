2cb2071400f080361a29a5152f4881b8f53d7c8b
Chris Williams <chris.williams@cloudscope.io>
2021-10-24 14:10:35 -0700

    Initial checkin

---
+++ tests/AppkitSharp.TestClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppkitSharp.TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
